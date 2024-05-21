cc7e82b5e71341f820a8f09a5733e6ba829baaf3
Dakarapu Pavitra <pavitra2128@gmail.com>
2024-05-21 11:50:32 +0530

    shadcn3

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
-
+module.exports = {
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        config.node = {
+          net: 'empty',
+        };
+      }
+      return config;
+    },
+  };
+  
 export default nextConfig;
