95e8b2053c13592a9f8911bb1281aaa3f92953eb
Dakarapu Pavitra <pavitra2128@gmail.com>
2024-05-21 12:13:23 +0530

    shadcn6

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-        webpack: (config, { isServer }) => {
-          if (!isServer) {
-            config.node = {
-              net: 'empty',
-              fs:'empty',
-            };
-          }
-          return config;
-        },
-      };
-      
-export default nextConfig;
+    webpack: (config, { isServer }) => {
+      if (!isServer && config.node) {
+        // Check if config.node is defined before accessing its properties
+        delete config.node.net;
+        delete config.node.fs;
+      }
+      return config;
+    },
+  };
+  
+  export default nextConfig;
+  
