ae101c07165944ebb02df77d77e6a4920ef75d85
akshayadesu <akshayadesu1@gmail.com>
2024-07-27 10:27:31 +0530

    sound

--- src/pages/NavBar.tsx
+++ src/pages/NavBar.tsx
@@ -1,18 +1,48 @@
-// Navbar.tsx
-import React, { useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { NavigationMenu, NavigationMenuList, NavigationMenuItem, NavigationMenuLink } from "@/components/ui/navigation-menu";
-import { ChevronDownIcon, ChevronUpIcon } from 'lucide-react';
+import { ChevronDownIcon, ChevronUpIcon, Volume2, VolumeX } from 'lucide-react';
 import { useRouter } from 'next/router';
 import Link from 'next/link'; // Import next/link for navigation
 
-const Navbar = ({}) => {
+const Navbar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false); // Assuming initially play the audio
+  const audioRef = useRef<HTMLAudioElement | null>(null);
   const router = useRouter();
 
+  // Function to toggle mobile menu
   const toggleMobileMenu = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
   };
 
+  // Function to toggle sound
+  const toggleSound = () => {
+    setIsPlaying(!isPlaying);
+  };
+
+  // Effect to play or pause audio based on isPlaying state
+  useEffect(() => {
+    if (!audioRef.current) {
+      audioRef.current = new Audio('/Shiva-Stotram.mp3');
+      audioRef.current.loop = true; // Loop the audio
+    }
+
+    if (isPlaying) {
+      audioRef.current.play().catch(error => {
+        console.error("Error attempting to play audio:", error);
+      });
+    } else {
+      audioRef.current.pause();
+    }
+
+    return () => {
+      if (audioRef.current) {
+        audioRef.current.pause();
+      }
+    };
+  }, [isPlaying]);
+
+  // Navigation links array
   const navigationLinks = [
     { label: 'Home', section: '/' },
     { label: 'About us', section: '/about' },
@@ -21,9 +51,10 @@ const Navbar = ({}) => {
     { label: 'News & Events', section: '/NewsAndEvents', external: true },
     { label: 'Our Temples', section: '/Temples' },
     { label: 'Donations', section: '/donations' },
-    { label: 'Contacts', section: '/contact-us' }, 
+    { label: 'Contacts', section: '/contact-us' },
   ];
 
+  // Render navigation links
   const renderLinks = (isMobile = false) => (
     <NavigationMenu>
       <NavigationMenuList className={isMobile ? 'flex flex-col items-center space-y-2' : 'flex space-x-6'}>
@@ -50,6 +81,9 @@ const Navbar = ({}) => {
           <div className="text-maroon text-xl sm:text-3xl font-hindi font-bold whitespace-nowrap">
             सारसबाग गणपति
           </div>
+          <button onClick={toggleSound} className="text-maroon">
+            {isPlaying ? <Volume2 size={36} /> : <VolumeX size={36} />}
+          </button>
         </div>
 
         <button className="lg:hidden text-gray-600 focus:outline-none" onClick={toggleMobileMenu}>
